9de8f1e5ccbe5c1b37928b087628473c18079623
Sho Maeda <syou20020911@gmail.com>
2022-10-27 21:46:47 +0900

    dev

--- resources/views/components/user/dropdown-button.blade.php
+++ resources/views/components/user/dropdown-button.blade.php
@@ -1,6 +1,13 @@
-@props(['event', 'text'])
+@props(['event', 'text', 'option', 'isChecked'])
 
-<span @click="{{ $event }} = !{{ $event }}"
-        {{ $attributes->merge(['class' => 'block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out']) }}>
-  {{ _($text) }}
-</span>
+@isset($option)
+  <span @click="{{ $event }} = !{{ $event }}; {{ $isChecked }} = true"
+          {{ $attributes->merge(['class' => 'block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out']) }}>
+    {{ _($text) }}
+  </span>
+@else
+  <span @click="{{ $event }} = !{{ $event }}"
+          {{ $attributes->merge(['class' => 'block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out']) }}>
+    {{ _($text) }}
+  </span>
+@endisset
--- resources/views/livewire/user/user-task/show.blade.php
+++ resources/views/livewire/user/user-task/show.blade.php
@@ -34,17 +34,13 @@
                     <div class="tabs bg-white">
                         <div class="border-b overflow-hidden">
                             <div class="border-l-2 border-transparent relative">
-                                <hearder class="flex flex-wrap pt-4 pl-8 pr-8">
-                                    {{-- <span class="flex-1 text-grey-darkest font-thin text-xl">タスク名</span> --}}
-                                    {{-- <span class="flex-1 text-grey-darkest font-thin text-xl">{{ $attribute->title }}</span> --}}
-                                    {{-- <div class='flex-1 flex-wrap mb-6'> --}}
-                                        <div class="flex-1 font-bold">タスク名</div>
-                                        <div class="flex-1">{{ $attribute->title }}</div>
-                                    {{-- </div> --}}
+                                <hearder class="flex flex-wrap pt-4 pl-10 pr-8">
+                                    <div class="flex-1 font-bold">タスク名</div>
+                                    <div class="flex-1 pl-16">{{ $attribute->title }}</div>
                                     <div class="flex-none sm:flex sm:items-center sm:ml-6">
-                                        <div class="flex-none rounded-full border border-grey flex items-center justify-center w-7 h-7 test">
+                                        <div class="flex-none rounded-full border border-grey flex items-center justify-center w-6 h-6 mr-4 test">
                                             <input @click="isChecked = !isChecked"
-                                           class="w-full absolute z-10 cursor-pointer opacity-0 h-5 top-6" type="checkbox" id="bt">
+                                           class="w-6 h-6 absolute cursor-pointer opacity-0" type="checkbox" id="bt">
                                             <svg aria-hidden="true" class="" data-reactid="266" fill="none"
                                                 height="24" stroke="#606F7B" stroke-linecap="round"
                                                 stroke-linejoin="round" stroke-width="2" viewbox="0 0 24 24" width="24"
@@ -52,18 +48,20 @@
                                                 <polyline points="6 9 12 15 18 9"></polyline>
                                             </svg>
                                         </div>
-                                        <x-dropdown align="right" width="48" original="-top-4 right-6">
-                                            <x-slot name="trigger">
-                                                <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">︙</button>
-                                            </x-slot>
-                                            <x-slot name="content">
-                                                <x-user.dropdown-button event="isChecked"
-                                                                        text="編集"
-                                                >
-                                                </x-user.dropdown-button>
-                                            </x-slot>
-                                        </x-dropdown>
                                     </div>
+                                    <x-dropdown align="right" width="48" original="-top-4 right-6">
+                                        <x-slot name="trigger">
+                                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">︙</button>
+                                        </x-slot>
+                                        <x-slot name="content">
+                                            <x-user.dropdown-button event="isEdit"
+                                                                    text="編集"
+                                                                    option="true"
+                                                                    isChecked="isChecked"
+                                            >
+                                            </x-user.dropdown-button>
+                                        </x-slot>
+                                    </x-dropdown>
                                 </hearder>
                                 <div class="tab-content max-h-0 m-1.5" :class="isChecked ? 'max-h-screen' : ''">
                                     <div class="pl-8 pr-8 pb-5 text-grey-darkest p-2">
@@ -92,48 +90,89 @@
                             <div class="border-l-2 border-transparent relative">
                                 <input @click="isChecked = !isChecked"
                                        class="w-full absolute z-10 cursor-pointer opacity-0 h-5 top-6" type="checkbox" id="bt">
-                                <header class="flex flex-wrap p-4 pl-8 pr-8 cursor-pointer select-none tab-label">
-                                    <span class="flex-1 text-grey-darkest font-thin text-xl">
-                                        {{ $attribute->title }}
-                                    </span>
-                                    <div class="flex-none rounded-full border border-grey flex items-center justify-center w-7 h-7 test">
-                                        <svg aria-hidden="true" class="" data-reactid="266" fill="none"
-                                            height="24" stroke="#606F7B" stroke-linecap="round"
-                                            stroke-linejoin="round" stroke-width="2" viewbox="0 0 24 24" width="24"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <polyline points="6 9 12 15 18 9"></polyline>
-                                        </svg>
-                                    </div>
-                                    <button @click="isEdit = !isEdit"
-                                            class="flex-none w-7 h-7">
-                                            ︙
-                                    </button>
-                                </header>
+                                    <hearder class="flex flex-wrap pt-4 pl-10 pr-8">
+                                        <div class="flex-1 font-bold">タスク名</div>
+                                        <div class="flex-1 pl-20">{{ $attribute->title }}</div>
+                                        <div class="flex-none sm:flex sm:items-center sm:ml-6">
+                                            <div class="flex-none rounded-full border border-grey flex items-center justify-center w-6 h-6 mr-4 test">
+                                                <input @click="isChecked = !isChecked"
+                                               class="w-6 h-6 absolute cursor-pointer opacity-0" type="checkbox" id="bt">
+                                                <svg aria-hidden="true" class="" data-reactid="266" fill="none"
+                                                    height="24" stroke="#606F7B" stroke-linecap="round"
+                                                    stroke-linejoin="round" stroke-width="2" viewbox="0 0 24 24" width="24"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <polyline points="6 9 12 15 18 9"></polyline>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                        <x-dropdown align="right" width="48" original="-top-4 right-6">
+                                            <x-slot name="trigger">
+                                                <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">︙</button>
+                                            </x-slot>
+                                            <x-slot name="content">
+                                                <x-user.dropdown-button event="isEdit"
+                                                                        text="編集"
+                                                                        option="true"
+                                                                        isChecked="isChecked"
+                                                >
+                                                </x-user.dropdown-button>
+                                            </x-slot>
+                                        </x-dropdown>
+                                    </hearder>
                                 <div class="tab-content max-h-0" :class="isChecked ? 'max-h-screen' : ''">
-                                    <x-user.input name="title" 
-                                                  id="title" 
-                                                  model="title"
-                                                  type="text" 
-                                                  placeholder="title"
-                                                  required="true"
-                                    ></x-user.input>
-                                    <x-user.input name="details" 
-                                                  id="details" 
-                                                  model="details"
-                                                  type="text" 
-                                                  placeholder="details"
-                                                  required="true"
-                                    ></x-user.input>
-                                    <x-user.input name="deadline" 
-                                                  id="deadline" 
-                                                  model="deadline"
-                                                  type="date" 
-                                                  placeholder="deadline"
-                                    ></x-user.input>
-                                    <x-user.button text="更新"
-                                                  emit="save-edit-confirm"
-                                                  type="submit"
-                                    ></x-user.button>
+                                    <div class='text-grey-darkest p-4 pl-10 flex flex-wrap flex-end'>
+                                        <div class="flex-1 font-bold">タイトル</div>
+                                        <div class="flex-1">
+                                            <x-user.input value="title"
+                                                          id="title" 
+                                                          model="title"
+                                                          type="text" 
+                                                          placeholder="Stripeの導入"
+                                                          required="true"
+                                            ></x-user.input>
+                                        </div>
+                                    </div>
+                                    <div class='text-grey-darkest p-4 pl-10 flex flex-wrap'>
+                                        <div class="flex-1 font-bold">詳細</div>
+                                        <div class="flex-1">
+                                            <x-user.input value="details"
+                                                          id="details" 
+                                                          model="details"
+                                                          type="text" 
+                                                          placeholder="フレームワーク：laravel, etc..."
+                                                          required="true"
+                                            ></x-user.input>
+                                        </div>
+                                    </div>
+                                    <div class='text-grey-darkest p-4 pl-10 flex flex-wrap'>
+                                        <div class="flex-1 font-bold">備考</div>
+                                        <div class="flex-1">
+                                            <x-user.input value="remarks"
+                                                          id="remarks" 
+                                                          model="remarks"
+                                                          type="text" 
+                                                          placeholder="セキュリティ高め"
+                                                          required="true"
+                                            ></x-user.input>
+                                        </div>
+                                    </div>
+                                    <div class='text-grey-darkest p-4 pl-10 flex flex-wrap'>
+                                        <div class="flex-1 font-bold">期限</div>
+                                        <div class="flex-1">
+                                            <x-user.input value="deadline"
+                                                          id="deadline" 
+                                                          model="deadline"
+                                                          type="date" 
+                                                          required="true"
+                                            ></x-user.input>
+                                        </div>
+                                    </div>
+                                    <div class="m-4">
+                                        <x-user.button text="更新"
+                                                       emit="save-edit-confirm"
+                                                       type="submit"
+                                        ></x-user.button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
