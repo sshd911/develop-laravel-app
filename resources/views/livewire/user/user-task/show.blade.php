538c0ce4120b7357688b58dccf686b6f626112ee
Sho Maeda <syou20020911@gmail.com>
2022-10-27 20:39:00 +0900

    dev

--- resources/views/livewire/user/user-task/show.blade.php
+++ resources/views/livewire/user/user-task/show.blade.php
@@ -34,38 +34,51 @@
                     <div class="tabs bg-white">
                         <div class="border-b overflow-hidden">
                             <div class="border-l-2 border-transparent relative">
-                                <header class="flex flex-wrap p-4 pl-8 pr-8 cursor-pointer select-none tab-label">
-                                    <span class="flex-1 text-grey-darkest font-thin text-xl">
-                                        {{ $attribute->title }}
-                                    </span>
-                                    <div class="flex-none rounded-full border border-grey flex items-center justify-center w-7 h-7 test">
-                                        <input @click="isChecked = !isChecked"
-                                       class="w-full absolute z-10 cursor-pointer opacity-0 h-5 top-6" type="checkbox" id="bt">
-                                        <svg aria-hidden="true" class="" data-reactid="266" fill="none"
-                                            height="24" stroke="#606F7B" stroke-linecap="round"
-                                            stroke-linejoin="round" stroke-width="2" viewbox="0 0 24 24" width="24"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <polyline points="6 9 12 15 18 9"></polyline>
-                                        </svg>
+                                <hearder class="flex flex-wrap pt-4 pl-8 pr-8">
+                                    {{-- <span class="flex-1 text-grey-darkest font-thin text-xl">タスク名</span> --}}
+                                    {{-- <span class="flex-1 text-grey-darkest font-thin text-xl">{{ $attribute->title }}</span> --}}
+                                    {{-- <div class='flex-1 flex-wrap mb-6'> --}}
+                                        <div class="flex-1 font-bold">タスク名</div>
+                                        <div class="flex-1">{{ $attribute->title }}</div>
+                                    {{-- </div> --}}
+                                    <div class="flex-none sm:flex sm:items-center sm:ml-6">
+                                        <div class="flex-none rounded-full border border-grey flex items-center justify-center w-7 h-7 test">
+                                            <input @click="isChecked = !isChecked"
+                                           class="w-full absolute z-10 cursor-pointer opacity-0 h-5 top-6" type="checkbox" id="bt">
+                                            <svg aria-hidden="true" class="" data-reactid="266" fill="none"
+                                                height="24" stroke="#606F7B" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-width="2" viewbox="0 0 24 24" width="24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <polyline points="6 9 12 15 18 9"></polyline>
+                                            </svg>
+                                        </div>
+                                        <x-dropdown align="right" width="48" original="-top-4 right-6">
+                                            <x-slot name="trigger">
+                                                <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">︙</button>
+                                            </x-slot>
+                                            <x-slot name="content">
+                                                <x-user.dropdown-button event="isChecked"
+                                                                        text="編集"
+                                                >
+                                                </x-user.dropdown-button>
+                                            </x-slot>
+                                        </x-dropdown>
                                     </div>
-                                    <button @click="isEdit = !isEdit"
-                                            class="flex-none w-7 h-7 cursor-help">
-                                            ︙
-                                    </button>
-                                </header>
-                                <div class="tab-content max-h-0" :class="isChecked ? 'max-h-screen' : ''">
-                                    <div class="pl-8 pr-8 pb-5 text-grey-darkest">
-                                        <ul class="pl-4">
-                                            <li class="pb-2">
-                                                {{ $attribute->title}}
-                                            </li>
-                                            <li class="pb-2">
-                                                {{ $attribute->detials}}
-                                            </li>
-                                            <li class="pb-2">
-                                                {{ $attribute->deadline}}
-                                            </li>
-                                        </ul>
+                                </hearder>
+                                <div class="tab-content max-h-0 m-1.5" :class="isChecked ? 'max-h-screen' : ''">
+                                    <div class="pl-8 pr-8 pb-5 text-grey-darkest p-2">
+                                        <div class='flex flex-wrap mb-6'>
+                                            <div class="flex-1 font-bold">表示名</div>
+                                            <div class="flex-1">{{ $attribute->details }}</div>
+                                        </div>
+                                        <div class='flex flex-wrap mb-6'>
+                                            <div class="flex-1 font-bold">詳細</div>
+                                            <div class="flex-1">{{ $attribute->deadline }}</div>
+                                        </div>
+                                        <div class='flex flex-wrap mb-6'>
+                                            <div class="flex-1 font-bold">備考</div>
+                                            <div class="flex-1">{{ $attribute->remarks }}</div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -130,3 +143,10 @@
         @endforeach
     @endif
 </div>
+<script>
+    function closeForm(){
+        setTimeout(() => {
+            this.isChecked = false;
+        }, 1300);
+    }
+</script>
