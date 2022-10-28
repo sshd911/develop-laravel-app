01ee9c82656721c6af2f6a751a7f6d1561811373
Sho Maeda <syou20020911@gmail.com>
2022-10-28 21:56:12 +0900

    div

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-gray-500 border-b border-lime-500">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-10 lg:px-8">
         <div class="flex justify-content items-center h-16">
@@ -12,22 +12,28 @@
 
                 <!-- Navigation Links -->
                 <div class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-white font-bold">
                         {{ __('ホーム') }}
                     </x-nav-link>
                 </div>
                 <div class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('user.userTask.index')">
+                    <x-nav-link :href="route('user.userTask.index')" 
+                                :active="request()->routeIs('user.userTask.index')"
+                                class="text-white font-bold">
                         {{ __('質問') }}
                     </x-nav-link>
                 </div>
                 <div class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('user.userRequest.index')">
+                    <x-nav-link :href="route('user.userRequest.index')" 
+                                :active="request()->routeIs('user.userRequest.index')"
+                                class="text-white font-bold">
                         {{ __('受託') }}
                     </x-nav-link>
                 </div>
                 <div class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('user.userProfile.index')">
+                    <x-nav-link :href="route('user.userProfile.index')" 
+                                :active="request()->routeIs('user.userProfile.index')"
+                                class="text-white font-bold">
                         {{ __('設定') }}
                     </x-nav-link>
                 </div>
@@ -38,10 +44,10 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name ?? 'ユーザー' }}</div>
+                            <div class="text-white font-bold">{{ Auth::user()->name ?? 'ユーザー' }}</div>
 
                             <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <svg class="fill-current h-4 w-4 text-white font-bold" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </div>
